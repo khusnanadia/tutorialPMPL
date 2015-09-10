22fe40b64442a9121899a14c1e8dc6eb5a51f021
Khusna Nadia <khusnanadia@gmail.com>
2015-09-10 03:58:56 +0000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -18,12 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+#	def check_for_now_row_in_list_table(self, row_text):
+#		table = self.browser.find_element_by_id('id_list_table')
+#		rows = table.find_elements_by_tag_name('tr')
+#		self.assertIn(row_text, [row.text for now in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+#		self.fail('Finish the test!')
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribbute('placeholder'),
+			'Enter a to-do item'
+		)
 
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers')
+		)
+		self.fail('Finish the test!')
 
 if __name__=='__main__':
 	unittest.main(warnings="ignore")
