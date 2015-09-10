6fbcb4050d65fda610830b2633913a98eba02445
Khusna Nadia <khusnanadia@gmail.com>
2015-09-10 03:41:15 +0000

    memulai tutorial 2

--- functional_tests.py
+++ functional_tests.py
@@ -15,14 +15,14 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-#	def tearDown(self):
-		#self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
 		self.assertIn('To-Do', self.browser.title)
-		#self.fail('Finish the test!')
+		self.fail('Finish the test!')
 
 
 if __name__=='__main__':
