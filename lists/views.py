7583d3b7fdaac0efc475ba6fb0c57a358836db23
Khusna Nadia <khusnanadia@gmail.com>
2015-09-12 04:43:39 +0000

    Commit bab 5 pertengahan sebelum refactor

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 #home_page = None
+
 def home_page(request):
-	#pass
-	#return HttpResponse('<html><title>To-Do lists</title><body><h1>Nama To-Do saya Khusna Nadia</h1></body></html>')
-	return render(request, 'home.html')
+#	pass
+#	return HttpResponse('<html><title>To-Do lists</title><body><h1>Nama To-Do saya Khusna Nadia</h1></body></html>')
+#	if request.method == 'POST':
+#		return HttpResponse(request.POST['item_text'])
+	return render(request, 'home.html',{
+		'new_item_text': request.POST.get('item_text', ''),
+	})
