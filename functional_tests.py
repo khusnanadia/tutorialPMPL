edd672a8340d0ac3751c40403e731e90fdc3a5a9
Khusna Nadia <khusnanadia@gmail.com>
2015-09-13 04:52:33 +0000

    three strikes and refactor bab 5

--- functional_tests.py
+++ functional_tests.py
@@ -20,10 +20,10 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-#	def check_for_now_row_in_list_table(self, row_text):
-#		table = self.browser.find_element_by_id('id_list_table')
-#		rows = table.find_elements_by_tag_name('tr')
-#		self.assertIn(row_text, [row.text for now in rows])
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -31,6 +31,9 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
+		
+#		inputbox.send_keys(Keys.ENTER)
+#		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 #		self.assertEqual(
@@ -40,6 +43,9 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys('Buy peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
 #		import time
 #		time.sleep(10)
